4c21dd32a655d5508e38b6243469457e27b4f0ad
Eric Clark <eclark0013@gmail.com>
2019-06-08 18:21:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,6 +5,9 @@ def fizzbuzz(input)
   if input%3 ==0 && input%5 == 0 
     return "FizzBuzz"
   elsif input%3 == 0 
-    return "fizz"
-  elsif
+    return "Fizz"
+  elsif input%5 == 0 
+    return "Buzz"
+  else
+    return
 end
